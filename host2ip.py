471710d5b0ae34f83be80a2c3a497fc26cac1ebe
Vishnu KS <ksvishnu56@gmail.com>
2017-04-23 22:41:03 +0530

    Script for converting list of host names to ip

---
+++ host2ip.py
@@ -0,0 +1,18 @@
+"""
+Created on Wed April 23 01:10:55 2017
+@author: vicz
+"""
+print '''
+ +-+-+-+-+ +-+ +-+-+ +-+-+-+-+-+-+-+-+-+                                           
+ |H|o|s|t| |2| |I|P| |C|o|n|v|e|r|t|e|r|                                           
+ +-+-+-+-+ +-+-+-+-+-+-+-+-+-+-+-+-+-+-+                                           
+           |b|y| |v|i|c|z|                                                         
+           +-+-+ +-+-+-+-+ 
+
+'''
+import socket
+
+hostnames=open('host.txt').readlines()
+
+for host in hostnames:
+    print host.strip(),socket.gethostbyname(host.strip())
